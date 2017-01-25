63b3fb1654d52c8caa8b97e9aecbbc0ef4041a51
Bentley Davis <Bentley@BentleyDavis.com>
2017-01-24 18:55:47 -0600

    two apps running

--- src/index.js
+++ src/index.js
@@ -3,21 +3,27 @@
 import { h, render } from 'preact';
 import './style';
 
-let root;
+let roots = [];
 function init() {
 	let App = require('./components/app').default;
-	root = render(<App />, document.body, root);
+	var statements = document.getElementsByTagName('statement');
+	//debugger;
+	for (var i = 0; i < statements.length; i++) {
+		roots[i] = render(<App />, statements[1], roots[i]);
+	}
+
+	//root = render(<App />, element, root);
 }
 
 // register ServiceWorker via OfflinePlugin, for prod only:
-if (process.env.NODE_ENV==='production') {
+if (process.env.NODE_ENV === 'production') {
 	require('./pwa');
 }
 
 // in development, set up HMR:
 if (module.hot) {
 	//require('preact/devtools');   // turn this on if you want to enable React DevTools!
-	module.hot.accept('./components/app', () => requestAnimationFrame(init) );
+	module.hot.accept('./components/app', () => requestAnimationFrame(init));
 }
 
 init();
