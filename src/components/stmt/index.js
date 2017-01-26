b756dae48a9612b2da174ff587da60b612b910b8
Bentley Davis <Bentley@BentleyDavis.com>
2017-01-26 13:10:13 -0600

    citation and select working

--- src/components/stmt/index.js
+++ src/components/stmt/index.js
@@ -9,14 +9,43 @@ export default class Stmt extends Component {
 		this.setState({ "isOpen": !this.state.isOpen })
 	};
 
+	toggleSelected = () => {
+		this.setState({ "isSelected": !this.state.isSelected })
+	};
+
 	render({ stmt, stmts, main }) {
 		//debugger;
 		return (
 			<li class={this.state.isOpen ? "open" : "closed"}>
-				<div class="statementPad notSelected">
+				<div onclick={this.toggleSelected} class={"statementPad " + (this.state.isSelected ? 'selected' : 'notSelected')}>
 					<div className={"statement " + (stmt.statement.isProMain ? 'pro' : 'con')}>
-						<span class="score">50%</span>
-						{main}:{stmt.id}:{stmt.statement.content}
+
+						<span class="score">
+							{main ?
+								Math.round(stmt.weightedPercentage * 100) + '%' :
+								Math.floor(Math.abs(stmt.weightDif))
+							}
+						</span>
+
+						{stmt.statement.content}&nbsp;
+
+						{(stmt.statement.citation.trim().length > 0 &&
+							stmt.statement.citationUrl.trim().length > 0) &&
+							<a target="_blank" href={stmt.statement.citationUrl}>
+								<span class="citation">{stmt.statement.citation}</span>
+							</a>
+						}
+						{(stmt.statement.citation.trim().length == 0 &&
+							stmt.statement.citationUrl.trim().length > 0) &&
+							<a target="_blank" href={stmt.statement.citationUrl}>
+								<span class="citation">{stmt.statement.citationUrl}</span>
+							</a>
+						}
+						{(stmt.statement.citation.trim().length > 0 &&
+							stmt.statement.citationUrl.trim().length == 0) &&
+							<span class="citation">{stmt.statement.citation}</span>
+						}
+
 					</div>
 					{stmt.statement.childIds.length > 0 &&
 						<div class="opener" onclick={this.toggleOpen}>▽</div>
